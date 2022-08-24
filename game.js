5e95e54610d871f8a7f431c72ffd09344dc9163d
Fabricio Dos Santos <fabricioperazzo@gmail.com>
2022-08-24 18:07:56 -0400

    Added getComputerChoice function, which generates a random integer 0, 1 or 2 and returns the move to play

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const ROCK = 0;
+const PAPER = 1;
+const SCISSORS = 2;
+
+function getComputerChoice() {
+    let randNum = Math.floor(Math.random() * 3);
+    if (randNum === 0) return ROCK;
+    if (randNum === 1) return PAPER;
+    if (randNum === 2) return SCISSORS;
+}
+
+console.log(getComputerChoice());
